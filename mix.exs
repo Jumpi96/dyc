d9fbc57a9345a702eceb437873f4180fc90e9117
Jumpi96 <juampilorenzo@gmail.com>
2019-01-29 14:57:07 -0300

    credo installed

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Dyc.MixProject do
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.post": :test,
         "coveralls.html": :test
       ],
@@ -27,9 +27,8 @@ defmodule Dyc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.5", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.5", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
     ]
   end
 end
