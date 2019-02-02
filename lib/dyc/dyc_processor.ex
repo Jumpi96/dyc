4955de949337c519657ce44ccda1d0b70c060d55
Jumpi96 <juampilorenzo@gmail.com>
2019-02-02 12:52:14 -0300

    Credo changes

--- lib/dyc/dyc_processor.ex
+++ lib/dyc/dyc_processor.ex
@@ -10,27 +10,27 @@ defmodule Dyc.DycProcessor do
     Return something.
     """  
     def process_files({code_path, csv_file}) do
-      pid = spawn(__MODULE__, :process_concurrently, [self(), {csv_file, code_path}, {}])
+      spawn(__MODULE__, :process_concurrently, [self(), {csv_file, code_path}, {}])
       receive do
         {:ok, data} -> data
       end
     end
 
-    def process_concurrently(caller, {csv_file, code_path}, {}) do
-      pid_csv_scraper = spawn(Dyc.UsageScraper, :scrap_file, [self(), csv_file, :csv])
+    def process_concurrently(caller, {csv_file, _code_path}, {}) do
+      spawn(Dyc.UsageScraper, :scrap_file, [self(), csv_file, :csv])
       # Here you should define the other process
       receive do
-        {pid_csv_scraper, usage_data} -> send caller, {:ok, usage_data}
+        {_, usage_data} -> send caller, {:ok, usage_data}
       end
     end
-    def process_concurrently(caller, {_, code_path}, {usage, nil}) do
+    def process_concurrently(caller, {_, _code_path}, {usage, nil}) do
       receive do
-        {sender, data} -> send caller, {usage, data}
+        {_, data} -> send caller, {usage, data}
       end
     end
-    def process_concurrently(caller, {csv_file, _}, {nil, code}) do
+    def process_concurrently(caller, {_csv_file, _}, {nil, code}) do
       receive do
-        {sender, data} -> send caller, {data, code}
+        {_, data} -> send caller, {data, code}
       end
     end
 
