2f1c0f2f1604adeb0c83081ec5802c47a26a7065
Jumpi96 <juampilorenzo@gmail.com>
2019-02-03 11:37:37 -0300

    Feature working

---
+++ lib/dyc/code_scraper.ex
@@ -0,0 +1,60 @@
+defmodule Dyc.CodeScraper do
+  @extensions %{python: ".py"}
+  @moduledoc """
+  Get the functions data from the input code and transform it to
+  the internal representation: [
+    ok: %{
+      "file" => "file.py",
+      "function" => "foo()",
+      "line" => "10"
+    }
+  ]
+  """
+    
+  @doc """
+  Receives a project path and the language of the code as an atom.
+  
+  Returns a list of maps.
+  """
+  def scrap_code(caller, path, language) do
+    result = scrap_folder(path, language)
+    send caller, {self(), result}
+  end
+
+  def scrap_folder(path, language) do
+    path
+      |> File.ls |> elem(1)
+      |> Enum.map(&"#{path}/#{&1}")
+      |> Enum.map(&scrap_path(&1, language))
+      |> Enum.reject(&is_nil/1)
+      |> List.flatten()
+  end
+
+  def scrap_path(path, language) do
+    if File.dir?(path) do
+      scrap_folder(path, language)
+    else
+      scrap_file(path, language)
+    end
+  end
+
+  def scrap_file(path, language) do
+    if String.ends_with?(path, @extensions[language]) do
+      scrap_code_file(path, language)
+    end
+  end
+
+  def scrap_code_file(path, :python) do
+    File.stream!(path) 
+      |> Stream.with_index
+      |> Stream.filter(fn ({line, _index}) -> String.contains?(line, "def ") end) 
+      |> Stream.map(fn ({line, index}) -> 
+        %{file: path, line: index, function: String.slice(line, index_of(line, "def") + 4..index_of(line, ")"))} end) 
+      |> Enum.to_list
+  end
+
+  def index_of(string, char) do
+    :binary.match(string, char) |> elem(0)
+  end
+
+end
--- lib/dyc/dyc_processor.ex
+++ lib/dyc/dyc_processor.ex
@@ -12,25 +12,26 @@ defmodule Dyc.DycProcessor do
     def process_files({code_path, csv_file}) do
       spawn(__MODULE__, :process_concurrently, [self(), {csv_file, code_path}, {}])
       receive do
-        {:ok, data} -> data
+        {usage_data, code_data} -> usage_data
       end
     end
 
-    def process_concurrently(caller, {csv_file, _code_path}, {}) do
-      spawn(Dyc.UsageScraper, :scrap_file, [self(), csv_file, :csv])
-      # Here you should define the other process
+    def process_concurrently(caller, {csv_file, code_path}, {}) do
+      usage_pid = spawn(Dyc.UsageScraper, :scrap_file, [self(), csv_file, :csv])
+      code_pid = spawn(Dyc.CodeScraper, :scrap_code, [self(), code_path, :python])
       receive do
-        {_, usage_data} -> send caller, {:ok, usage_data}
+        {usage_pid, usage_data} -> process_concurrently(caller, {usage_data, nil})
+        {code_pid, code_data} -> process_concurrently(caller, {nil, code_data})
       end
     end
-    def process_concurrently(caller, {_, _code_path}, {usage, nil}) do
+    def process_concurrently(caller, {usage, nil}) do
       receive do
-        {_, data} -> send caller, {usage, data}
+        {_, code_data} -> send caller, {usage, code_data}
       end
     end
-    def process_concurrently(caller, {_csv_file, _}, {nil, code}) do
+    def process_concurrently(caller, {nil, code}) do
       receive do
-        {_, data} -> send caller, {data, code}
+        {_, usage_data} -> send caller, {usage_data, code}
       end
     end
 
