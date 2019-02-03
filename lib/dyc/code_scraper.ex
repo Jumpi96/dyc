504f7692c3b2bf213a9e1402bf17fb670cccc9ef
Jumpi96 <juampilorenzo@gmail.com>
2019-02-03 12:55:45 -0300

    Adding docs

--- lib/dyc/code_scraper.ex
+++ lib/dyc/code_scraper.ex
@@ -21,6 +21,11 @@ defmodule Dyc.CodeScraper do
     send caller, {:code, result}
   end
 
+  @doc """
+  Receives a folder path and the language of the code as an atom.
+  
+  Returns a list of maps with the internal representation.
+  """
   def scrap_folder(path, language) do
     path
       |> File.ls |> elem(1)
@@ -30,6 +35,11 @@ defmodule Dyc.CodeScraper do
       |> List.flatten()
   end
 
+  @doc """
+  Receives a path with unknown type path and language as an atom.
+  
+  Returns a file or folder scrapped depending on the type.
+  """
   def scrap_path(path, language) do
     if File.dir?(path) do
       scrap_folder(path, language)
@@ -38,12 +48,23 @@ defmodule Dyc.CodeScraper do
     end
   end
 
+  @doc """
+  Receives a file path and the language of the code as an atom.
+  
+  Returns a list of maps with functions if it was a code file.
+  """
   def scrap_file(path, language) do
     if String.ends_with?(path, @extensions[language]) do
       scrap_code_file(path, language)
     end
   end
 
+  @doc """
+  Receives a file path and the language of the code as an atom.
+  Uses Stream to read the file, filter the functions and maps its information.
+
+  Returns a list of maps with functions.
+  """
   def scrap_code_file(path, :python) do
     path
       |> File.stream!
@@ -58,5 +79,8 @@ defmodule Dyc.CodeScraper do
       |> Enum.to_list
   end
 
+  @doc """
+  Wrapper to get the index of a char/string inside another string.
+  """
   def index_of(string, char), do: string |> :binary.match(char) |> elem(0)
 end
